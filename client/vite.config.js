9bad325876d7a2f8d3e87f33c325393c2a87d4a3
danuapratama <danuaguspratama98@gmail.com>
2024-09-08 00:34:38 +0700

    Integrate Progressive Web App (PWA) with React and Vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,40 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   base: '/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        skipWaiting: true,
+        clientsClaim: true,
+      },
+      manifest: {
+        name: 'Dibimovie',
+        short_name: 'Dibimovie',
+        description: 'A Progressive Web App for browsing and discovering movies or TV shows. Built with React, Vite, and powered by the TMDB API. Created with love by danutama.github.io',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/logo192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/logo.png',
+            sizes: '500x500',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     proxy: {
       '/api': 'http://localhost:5001',
